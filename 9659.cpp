8c8e089481dd8bfa0e3ad36bbd245cc5a74bc2a7
sejinik <sejinik@naver.com>
2018-05-25 01:53:53 +0900

    Remove all files

--- 9659.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include <iostream>
-using namespace std;
-#define ll long long
-int main() {
-	ll n; scanf(" %lld", &n);
-	printf("%s\n", (n & 1) ? "SK" : "CY");
-}
--- 9660.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include <iostream>
-using namespace std;
-#define ll long long
-int main() {
-	ll n; scanf(" %lld", &n);
-	if (n % 7 == 0 || n % 7 == 2) puts("CY");
-	else puts("SK");
-}
